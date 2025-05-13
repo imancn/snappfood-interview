cd8510cc2580b0d592084a5598bd9561577511c1
Iman Soltani <iman.soltani.j@gmail.com>
2025-05-13 21:24:48 +0330

    Initiate Springboot

---
+++ src/main/kotlin/com/cn/iman/snappfood/interview_task/InterviewTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.cn.iman.snappfood.interview_task
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InterviewTaskApplication
+
+fun main(args: Array<String>) {
+	runApplication<InterviewTaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/cn/iman/snappfood/interview_task/InterviewTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cn.iman.snappfood.interview_task
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InterviewTaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
